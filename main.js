086dcda2a4b84809cd3a8ddf341035bb494ef4d7
Leavon Parrish <novael@Leavons-MBP.home>
2018-02-13 22:10:31 -0500

    new font, changed header

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   }
 });
+
+document.addEventListener("scroll", function(event){
+  var scroll = event.pageY;
+  var header = document.getElementById("main-header");
+  var secondaryHeader = document.getElementById("secondary-header");
+
+  if(scroll >= 50) {
+    if(!header.classList.contains("scroll-header")){
+      header.classList.add("scroll-header");
+    }
+
+    // if(!secondaryHeader.classList.contains("scroll-header")){
+    //   secondaryHeader.classList.add("scroll-header");
+    // }
+  }
+  else {
+    header.classList.remove("scroll-header");
+    // secondaryHeader.classList.remove("scroll-header");
+  }
+});
